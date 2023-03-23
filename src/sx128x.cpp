a5d5be9faa18896c083f18a70b0648a10a31af99
JLP <jlpoltrack@gmail.com>
2023-03-23 09:47:25 -0400

    Version for Testing

--- src/sx128x.cpp
+++ src/sx128x.cpp
@@ -301,24 +301,24 @@ uint8_t buf[3];
 void Sx128xDriverBase::GetPacketStatus(int8_t* RssiSync, int8_t* Snr)
 {
 uint8_t status[5];
-int16_t rssi_sync_temp;
-int16_t snr_temp;
-int16_t neg_offset;
+int8_t rssi_sync_temp;
+int8_t snr_temp;
+int8_t neg_offset;
 
 // param 3 & 4 & 5 are not used, must read them nevertheless
 
     ReadCommand(SX1280_CMD_GET_PACKET_STATUS, status, 5);
 
-    rssi_sync_temp = -(int16_t)(int8_t)(status[0] / 2);
-    snr_temp = (int16_t)(int8_t)status[1];
+    rssi_sync_temp = -(int8_t)(status[0] / 2);
+    snr_temp = (int8_t)status[1];
 
     neg_offset = (snr_temp < 0) ? (snr_temp / 4) : 0;
 
-    rssi_sync_temp = ((rssi_sync_temp + neg_offset) >= INT8_MIN) ? (rssi_sync_temp + neg_offset) : INT8_MIN
-     
-    *RssiSync = (int8_t)rssi_sync_temp;
+    rssi_sync_temp += neg_offset;
 
-    *Snr = (int8_t)snr_temp / 4;
+    *RssiSync = rssi_sync_temp;
+
+    *Snr = snr_temp / 4;
 
     // datasheet p. 93, table 11-66: If the SNR ≤ 0, RSSI_{packet, real} = RSSI_{packet,measured} – SNR_{measured}
     // datasheet p.134, point 4: Actual SNR in dB =SnrPkt/4 , noting that only negative values should be used.
