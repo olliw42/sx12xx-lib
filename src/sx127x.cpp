b1bc5185771890406ea4ab03b979f03291bc3e9e
JLP <jlpoltrack@gmail.com>
2023-11-07 13:22:46 -0500

    LoRa update

--- src/sx127x.cpp
+++ src/sx127x.cpp
@@ -143,10 +143,10 @@ void Sx127xDriverBase::OptimizeSensitivity(uint8_t Bandwidth)
     // see errata SX1276_77_8_ErrataNote_1.1_STD.pdf, 2.1, page 4
 
     if (Bandwidth == SX1276_LORA_BW_500) {
-        WriteRegister(SX1276_REG_HighBWOptimize1, 0x02); // reg 0x36
-        WriteRegister(SX1276_REG_HighBWOptimize2, 0x64); // reg 0x3A
+        WriteRegister(SX1276_REG_LORA_HighBWOptimize1, 0x02); // reg 0x36
+        WriteRegister(SX1276_REG_LORA_HighBWOptimize2, 0x64); // reg 0x3A
     } else {
-        WriteRegister(SX1276_REG_HighBWOptimize1, 0x03); // 0x36
+        WriteRegister(SX1276_REG_LORA_HighBWOptimize1, 0x03); // 0x36
     }
 }
 
@@ -158,39 +158,39 @@ void Sx127xDriverBase::OptimizeReceiverResponse(uint8_t Bandwidth)
     //TODO: offset Frf ???
 
     if (Bandwidth == SX1276_LORA_BW_500) {
-        ReadWriteRegister(SX1276_REG_DetectOptimize, 0x80, SX1276_LORA_AUTOMATIC_IF_ON);
+        ReadWriteRegister(SX1276_REG_LORA_DetectOptimize, 0x80, SX1276_LORA_AUTOMATIC_IF_ON);
     } else {
-        ReadWriteRegister(SX1276_REG_DetectOptimize, 0x80, SX1276_LORA_AUTOMATIC_IF_OFF);
+        ReadWriteRegister(SX1276_REG_LORA_DetectOptimize, 0x80, SX1276_LORA_AUTOMATIC_IF_OFF);
     }
 
     switch (Bandwidth) {
     case SX1276_LORA_BW_7p8:
-        WriteRegister(SX1276_REG_0x2F, 0x48);
+        WriteRegister(SX1276_REG_LORA_0x2F, 0x48);
         break;
     case SX1276_LORA_BW_10p4:
     case SX1276_LORA_BW_15p6:
     case SX1276_LORA_BW_20p8:
     case SX1276_LORA_BW_31p25:
     case SX1276_LORA_BW_41p7:
-        WriteRegister(SX1276_REG_0x2F, 0x44);
+        WriteRegister(SX1276_REG_LORA_0x2F, 0x44);
         break;
     case SX1276_LORA_BW_62p5:
     case SX1276_LORA_BW_125:
     case SX1276_LORA_BW_250:
-        WriteRegister(SX1276_REG_0x2F, 0x40);
+        WriteRegister(SX1276_REG_LORA_0x2F, 0x40);
         break;
     }
 
     if (Bandwidth != SX1276_LORA_BW_500) {
-        WriteRegister(SX1276_REG_0x30, 0x00);
+        WriteRegister(SX1276_REG_LORA_0x30, 0x00);
     }
 }
 
 
 void Sx127xDriverBase::SetBufferBaseAddress(uint8_t txBaseAdress, uint8_t rxBaseAdress)
 {
-    WriteRegister(SX1276_REG_FifoTxBaseAddr, txBaseAdress);
-    WriteRegister(SX1276_REG_FifoRxBaseAddr, rxBaseAdress);
+    WriteRegister(SX1276_REG_LORA_FifoTxBaseAddr, txBaseAdress);
+    WriteRegister(SX1276_REG_LORA_FifoRxBaseAddr, rxBaseAdress);
 }
 
 
@@ -199,28 +199,28 @@ void Sx127xDriverBase::SetModulationParams(uint8_t SpreadingFactor, uint8_t Band
     // 7-4 Bw, 3-1 CodingRate, 0 ImplicitHeaderModeOn
     if (SpreadingFactor == SX1276_LORA_SF6) {
         // special treatment for SF6, see datasheet, p.27,  for SF6 the header must be implicit
-        WriteRegister(SX1276_REG_ModemConfig1, Bandwidth | CodingRate | SX1276_LORA_HEADER_IMPLICIT);
+        WriteRegister(SX1276_REG_LORA_ModemConfig1, Bandwidth | CodingRate | SX1276_LORA_HEADER_IMPLICIT);
     } else {
-        ReadWriteRegister(SX1276_REG_ModemConfig1, 0xFE, Bandwidth | CodingRate);
+        ReadWriteRegister(SX1276_REG_LORA_ModemConfig1, 0xFE, Bandwidth | CodingRate);
     }
 
     // 7-4 SpreadingFactor, 3 TxContinuousMode, 2 RxPayloadCrcOn, 1-0 SymbTimeout(9:8)
     if (SpreadingFactor == SX1276_LORA_SF6) {
         // special treatment for SF6, see datasheet, p.27, for SF6 the crc must be disabled
-        ReadWriteRegister(SX1276_REG_ModemConfig2, 0xF4, SpreadingFactor| SX1276_LORA_CRC_DISABLE);
+        ReadWriteRegister(SX1276_REG_LORA_ModemConfig2, 0xF4, SpreadingFactor| SX1276_LORA_CRC_DISABLE);
     } else {
-        ReadWriteRegister(SX1276_REG_ModemConfig2, 0xF0, SpreadingFactor);
+        ReadWriteRegister(SX1276_REG_LORA_ModemConfig2, 0xF0, SpreadingFactor);
     }
 
     // special treatment for SF6, see datasheet, p.27
     if (SpreadingFactor == SX1276_LORA_SF6) {
           // 7 AutomaticIFOn, 2-0 DetectionOptimize
-          ReadWriteRegister(SX1276_REG_DetectOptimize, 0x07, SX1276_LORA_DETECTION_OPTIMIZE_SF_6);
-          WriteRegister(SX1276_REG_DetectionThreshold, SX1276_LORA_DETECTION_TRESHOLD_SF_6);
+          ReadWriteRegister(SX1276_REG_LORA_DetectOptimize, 0x07, SX1276_LORA_DETECTION_OPTIMIZE_SF_6);
+          WriteRegister(SX1276_REG_LORA_DetectionThreshold, SX1276_LORA_DETECTION_TRESHOLD_SF_6);
     } else {
           // 7 AutomaticIFOn, 2-0 DetectionOptimize
-          ReadWriteRegister(SX1276_REG_DetectOptimize, 0x07, SX1276_LORA_DETECTION_OPTIMIZE_SF_7_12);
-          WriteRegister(SX1276_REG_DetectionThreshold, SX1276_LORA_DETECTION_TRESHOLD_SF_7_12);
+          ReadWriteRegister(SX1276_LORA_REG_DetectOptimize, 0x07, SX1276_LORA_DETECTION_OPTIMIZE_SF_7_12);
+          WriteRegister(SX1276_REG_LORA_DetectionThreshold, SX1276_LORA_DETECTION_TRESHOLD_SF_7_12);
     }
 }
 
@@ -228,21 +228,21 @@ void Sx127xDriverBase::SetModulationParams(uint8_t SpreadingFactor, uint8_t Band
 void Sx127xDriverBase::SetPacketParams(uint8_t PreambleLength, uint8_t HeaderType, uint8_t PayloadLength, uint8_t Crc, uint8_t InvertIQ)
 {
     // preamble length can be > 255, but we don't support this
-    WriteRegister(SX1276_REG_PreambleMsb, 0);
-    WriteRegister(SX1276_REG_PreambleLsb, PreambleLength);
+    WriteRegister(SX1276_REG_LORA_PreambleMsb, 0);
+    WriteRegister(SX1276_REG_LORA_PreambleLsb, PreambleLength);
 
     // 7-4 Bw, 3-1 CodingRate, 0 ImplicitHeaderModeOn
     // ATTENTION: for SF6 the header must be implicit
-    ReadWriteRegister(SX1276_REG_ModemConfig1, 0x01, HeaderType);
+    ReadWriteRegister(SX1276_REG_LORA_ModemConfig1, 0x01, HeaderType);
 
     // 7-4 SpreadingFactor, 3 TxContinuousMode, 2 RxPayloadCrcOn, 1-0 SymbTimeout(9:8)
     // ATTENTION: for SF6 the crc must be off
-    ReadWriteRegister(SX1276_REG_ModemConfig2, 0x04, Crc);
+    ReadWriteRegister(SX1276_REG_LORA_ModemConfig2, 0x04, Crc);
 
-    WriteRegister(SX1276_REG_PayloadLength, PayloadLength);
+    WriteRegister(SX1276_REG_LORA_PayloadLength, PayloadLength);
 
     // 6 InvertIQ RX, 0 InvertIQ TX
-    ReadWriteRegister(SX1276_REG_InvertIQ, 0x41, InvertIQ);
+    ReadWriteRegister(SX1276_REG_LORA_InvertIQ, 0x41, InvertIQ);
 
     //RegInvertIQ2         ???
     // Set to 0x19 for inverted IQ
@@ -253,7 +253,7 @@ void Sx127xDriverBase::SetPacketParams(uint8_t PreambleLength, uint8_t HeaderTyp
 void Sx127xDriverBase::SetDioIrqParams(uint8_t IrqMask, uint8_t Dio0Mask, uint8_t Dio1Mask)
 {
     // setting a bit masks the corresponding IRQ in RegIrqFlags
-    WriteRegister(SX1276_REG_IrqFlagsMask, ~IrqMask);
+    WriteRegister(SX1276_REG_LORA_IrqFlagsMask, ~IrqMask);
 
     // 7-6 Dio0Mapping, 5-4 Dio1Mapping, 3-2 Dio2Mapping, 1-0 Dio3Mapping
     WriteRegister(SX1276_REG_DioMapping1, Dio0Mask | Dio1Mask);
@@ -265,14 +265,14 @@ void Sx127xDriverBase::SetDioIrqParams(uint8_t IrqMask, uint8_t Dio0Mask, uint8_
 
 uint16_t Sx127xDriverBase::GetIrqStatus(void)
 {
-    return ReadRegister(SX1276_REG_IrqFlags);
+    return ReadRegister(SX1276_REG_LORA_IrqFlags);
 }
 
 
 void Sx127xDriverBase::ClearIrqStatus(uint8_t IrqMask)
 {
     // writing a 1 clears the IRQ
-    WriteRegister(SX1276_REG_IrqFlags, IrqMask);
+    WriteRegister(SX1276_REG_LORA_IrqFlags, IrqMask);
 }
 
 
@@ -331,22 +331,22 @@ void Sx127xDriverBase::SetRxTimeout(uint16_t tmo_symbols)
     if (tmo_symbols >= 1024) tmo_symbols = 1023; // only 10 bits
 
     // 7-4 SpreadingFactor, 3 TxContinuousMode, 2 RxPayloadCrcOn, 1-0 SymbTimeout(9:8)
-    ReadWriteRegister(SX1276_REG_ModemConfig2, 0x03, (tmo_symbols >> 8));
-    WriteRegister(SX1276_REG_SymbTimeoutLsb, (tmo_symbols & 0x00FF));
+    ReadWriteRegister(SX1276_REG_LORA_ModemConfig2, 0x03, (tmo_symbols >> 8));
+    WriteRegister(SX1276_REG_LORA_SymbTimeoutLsb, (tmo_symbols & 0x00FF));
 }
 
 
 void Sx127xDriverBase::GetPacketStatus(int16_t* RssiSync, int8_t* Snr)
 {
-    *RssiSync = (int16_t)-157 + ReadRegister(SX1276_REG_PktRssiValue);
-    *Snr = (int8_t)ReadRegister(SX1276_REG_PktSnrValue) / 4;
+    *RssiSync = (int16_t)-157 + ReadRegister(SX1276_REG_LORA_PktRssiValue);
+    *Snr = (int8_t)ReadRegister(SX1276_REG_LORA_PktSnrValue) / 4;
 }
 
 
 void Sx127xDriverBase::GetRxBufferStatus(uint8_t* rxPayloadLength, uint8_t* rxStartBufferPointer)
 {
-    *rxPayloadLength = ReadRegister(SX1276_REG_RxNbBytes);
-    *rxStartBufferPointer = ReadRegister(SX1276_REG_FifoRxCurrentAddr);
+    *rxPayloadLength = ReadRegister(SX1276_REG_LORA_RxNbBytes);
+    *rxStartBufferPointer = ReadRegister(SX1276_REG_LORA_FifoRxCurrentAddr);
 }
 
 
@@ -360,7 +360,7 @@ uint8_t Sx127xDriverBase::GetFirmwareRev(void)
 
 void Sx127xDriverBase::SetSyncWord(uint8_t SyncWord)
 {
-    WriteRegister(SX1276_REG_SyncWord, SyncWord);
+    WriteRegister(SX1276_REG_LORA_SyncWord, SyncWord);
 }
 
 
@@ -370,7 +370,7 @@ int32_t Sx127xDriverBase::GetFreqError(void)
 {
 uint8_t buf[3];
 
-    ReadRegister(SX1276_REG_FeiMsb, buf, 3);
+    ReadRegister(SX1276_REG_LORA_FeiMsb, buf, 3);
 
     uint32_t ufei = ((uint32_t)(buf[0] & 0b1111) << 16) + ((uint32_t)buf[1] << 8) + buf[2];
     int32_t fei = ufei;
@@ -413,7 +413,7 @@ void Sx127xDriverBase::AfcDo(void)
 {
     if (!_rf_freq_reg) return; // not initialized
 
-    uint8_t fei_msb = ReadRegister(SX1276_REG_FeiMsb);
+    uint8_t fei_msb = ReadRegister(SX1276_REG_LORA_FeiMsb);
 
     if (fei_msb & 0b1000) {
         // sign is negative
@@ -430,7 +430,7 @@ void Sx127xDriverBase::AfcDo(void)
     // adjust data rate, see datasheet
     // could avoid write when unchanged
     int8_t ppm_correction = (_rf_freq_reg_correction * 950000) / _rf_freq_reg;
-    WriteRegister(SX1276_REG_0x27, ppm_correction);
+    WriteRegister(SX1276_REG_LORA_0x27, ppm_correction);
 }
 
 
